1edd119770b846aa5fd3fac099ec3f022443715f
Jason Zubiate <jason@Jasons-Mac-mini.local>
2023-03-24 09:49:34 -0700

    work on header project

--- components/ProjectHeader.tsx
+++ components/ProjectHeader.tsx
@@ -1,9 +1,41 @@
 import styles from "@/styles/ProjectHeader.module.css";
+import Link from "next/link";
 
 export default function ProjectHeader({ project }) {
-	return (
-		<div className={styles.header}>
-			<h1>{project.name}</h1>
-		</div>
-	);
+  return (
+    <div className={styles.header}>
+      <h1>{project.h1}</h1>
+      <div className={styles.project_description}>
+        <label className={styles.label}>Description</label>
+        <p className={styles.p}>
+          Managed a team of designers during a comprehensive user testing
+          session for a mobile application focused on nutritional health
+          management, involving a sizeable cohort of 100 participants.
+        </p>
+      </div>
+      <div className={styles.info_row}>
+        <div className={styles.info}>
+          <label className={styles.label}>Role</label>
+          <p className={styles.p}>UX Lead, Frontend Developer</p>
+        </div>
+        <div className={styles.info}>
+          <label className={styles.label}>Timeline</label>
+          <p className={styles.p}>
+            June - September 2022 (12 weeks) January - Present
+          </p>
+        </div>
+        <div className={styles.info}>
+          <label className={styles.label}>Tools</label>
+          <p className={styles.p}>Figma, Miro, React, Next.js, Smartlook</p>
+        </div>
+        <div className={styles.disclaimer}>
+          <p className={styles.italic_p}>
+            This project is currently protected by an NDA. If you’d like access
+            please request a password.
+          </p>
+					<button className={styles.btn}>View Project</button>
+        </div>
+      </div>
+    </div>
+  );
 }
--- pages/nutripair.tsx
+++ pages/nutripair.tsx
@@ -1,24 +1,28 @@
 import fsPromises from "fs/promises";
 import path from "path";
+import Image from "next/image";
+import ProjectHeader from "@/components/ProjectHeader"
 
-export const getStaticProps = async () => {
-	const filePath = path.join(process.cwd(), "json/work.json");
-	const jsonData: any = await fsPromises.readFile(filePath);
-	const objectData = JSON.parse(jsonData);
-  console.log(objectData)
-
-	return {
-		props: objectData[0],
-	};
-};
-
-type nutripairProps = {
-  project: any
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "json/work.json");
+  const fileContents = await fsPromises.readFile(filePath, "utf8");
+  const data = JSON.parse(fileContents);
+  const nutripair = data.work.find((project) => project.url === "nutripair");
+  return {
+    props: {
+      project: nutripair,
+    },
+  };
 }
 
-export default function nutripair({project}:nutripairProps) {
+
+export default function nutripair({project}) {
   console.log(project)
   return (
-    <div>nutripair</div>
+    <div>
+      <Image src={`/${project.img}`} alt="Nutripair Logo" fill/>
+      <ProjectHeader project={project}/>
+    </div>
   )
 }
+
