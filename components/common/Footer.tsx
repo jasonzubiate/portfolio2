ecf9c767efa1f5785ca10fb8e2d86af7e0de4802
jasonzubiate <jzubiate@uci.edu>
2023-03-29 16:07:44 -0700

    media for footer

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -1,26 +1,38 @@
-import styles from '@/styles/Footer.module.css'
+import styles from "@/styles/Footer.module.css";
 
 export default function Footer() {
-  return (
-    <div className={styles.footer}>
-      <div className={styles.label}>
-        <label className={styles.label_title}>Contact</label>
-        <label className={styles.label_content}>jzubiate@uci.edu</label>
-      </div>
-      <div className={styles.label}>
-        <label className={styles.label_title}>Connect</label>
-        <label className={styles.label_content}>
-          <a className={styles.link} href="https://www.linkedin.com/in/jasonzubiate/">Linkedin</a>
-          <span> / </span>
-          <a className={styles.link} href="https://www.instagram.com/jason.zubiate/">Instagram</a>
-          <span> / </span>
-          <a className={styles.link} href="https://github.com/jasonzubiate?tab=repositories">Github</a>
-        </label>
-      </div>
-      <div className={styles.label}>
-        <label className={styles.label_title}>Based In</label>
-        <label className={styles.label_content}>Irvine, California</label>
-      </div>
-    </div>
-  )
+	return (
+		<div className={styles.footer}>
+			<div className={styles.label}>
+				<label className={styles.label_title}>Contact</label>
+				<label className={styles.label_content}>jzubiate@uci.edu</label>
+			</div>
+			<div className={styles.label}>
+				<label className={styles.label_title}>Connect</label>
+				<label className={styles.label_content}>
+					<a
+						className={styles.link}
+						href="https://www.linkedin.com/in/jasonzubiate/"
+					>
+						Linkedin
+					</a>
+					<span> / </span>
+					<a className={styles.link} href="https://dribbble.com/jasonzubiate">
+						Dribbble
+					</a>
+					<span> / </span>
+					<a
+						className={styles.link}
+						href="https://github.com/jasonzubiate?tab=repositories"
+					>
+						Github
+					</a>
+				</label>
+			</div>
+			<div className={styles.label}>
+				<label className={styles.label_title}>Based In</label>
+				<label className={styles.label_content}>Irvine, California</label>
+			</div>
+		</div>
+	);
 }
