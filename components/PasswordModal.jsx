adc0096ee1aca5037c2ab2d10a3c619bc1d26e75
jasonzubiate <jzubiate@uci.edu>
2023-04-19 10:55:56 -0700

    password added

--- components/PasswordModal.jsx
+++ components/PasswordModal.jsx
@@ -2,10 +2,23 @@ import React, { useState } from "react";
 import styles from "@/styles/PasswordModal.module.css";
 
 export default function PasswordModal({ setModalState }) {
+	const [password, setPassword] = useState("");
+
+	function handleSubmit() {
+		const correctPassword = "jz90521308"; // change this to your actual password
+		if (password === correctPassword) {
+			// do something if password is correct
+      setModalState(false)
+		} else {
+			// show an error message if password is incorrect
+			alert("Incorrect password. Please try again.");
+		}
+	}
+
 	return (
 		<div className={styles.background}>
 			<div className={styles.modal}>
-				<h2>Protected Page</h2>
+				<h2 className={styles.h2}>Protected Page</h2>
 				<p className={styles.p_italic}>
 					This project is currently protected by an NDA. If you’d like access
 					please{" "}
@@ -20,9 +33,14 @@ export default function PasswordModal({ setModalState }) {
 					className={styles.input}
 					type="text"
 					placeholder="Enter password"
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
 				/>
 				<div className={styles.btn_group}>
-					<button className={`${styles.btn} ${styles["primary-cta"]}`}>
+					<button
+						className={`${styles.btn} ${styles["primary-cta"]}`}
+						onClick={handleSubmit}
+					>
 						Submit
 					</button>
 					<button
