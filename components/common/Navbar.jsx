f21faa0832f4a4d9ba50bc907ee85dc8e2e6b80b
Jason Zubiate <jason@Jasons-Mac-mini.local>
2023-06-07 13:22:26 -0700

    more animations

--- components/common/Navbar.jsx
+++ components/common/Navbar.jsx
@@ -8,87 +8,90 @@ import Image from "next/image";
 import { gsap } from "gsap";
 
 export default function Navbar({ toggle, setToggle }) {
-	const [currentPage, setCurrentPage] = useState("work");
-	const navRef = useRef();
+  const [currentPage, setCurrentPage] = useState("work");
+  const navRef = useRef();
 
-	const handleNavClick = (page) => {
-		setCurrentPage(page);
-	};
+  const handleNavClick = (page) => {
+    setCurrentPage(page);
+  };
 
-	useEffect(() => {
-		let tl = gsap.timeline({
-			defaults: { ease: "power2", duration: 1.2, delay: 0.1 },
-		});
-		tl.to(navRef.current, {
-			height: 100,
-			opacity: 1,
-		});
-	});
+  useEffect(() => {
+    let tl = gsap.timeline({
+      defaults: { ease: "power2", duration: 1.2, delay: 0.1 },
+    });
 
-	return (
-		<nav className={styles.navbar} ref={navRef}>
-			<div className={styles.navbar_icon}>
-				<Link
-					className={styles.logo}
-					href={"/"}
-					onClick={() => handleNavClick("work")}
-				>
-					Jason Zubiate
-				</Link>
-			</div>
-			<div className={styles.navlinks}>
-				<NavItem
-					to={"/"}
-					label={"Work"}
-					current={currentPage === "work"}
-					onClick={() => handleNavClick("work")}
-				/>
-				<NavItem
-					to={"/self"}
-					label={"Self"}
-					current={currentPage === "self"}
-					onClick={() => handleNavClick("self")}
-				/>
-				{/* <NavItem
+    const viewportWidth = window.innerWidth;
+    const desiredHeight = viewportWidth >= 768 ? 100 : 64;
+
+    tl.to(navRef.current, {
+      height: desiredHeight,
+    });
+  }, []);
+
+  return (
+    <nav className={styles.navbar} ref={navRef}>
+      <div className={styles.navbar_icon}>
+        <Link
+          className={styles.logo}
+          href={"/"}
+          onClick={() => handleNavClick("work")}
+        >
+          Jason Zubiate
+        </Link>
+      </div>
+      <div className={styles.navlinks}>
+        <NavItem
+          to={"/"}
+          label={"Work"}
+          current={currentPage === "work"}
+          onClick={() => handleNavClick("work")}
+        />
+        <NavItem
+          to={"/self"}
+          label={"Self"}
+          current={currentPage === "self"}
+          onClick={() => handleNavClick("self")}
+        />
+        {/* <NavItem
 					to={"/music"}
 					label={"Music"}
 					current={currentPage === 'music'}
 					onClick={() => handleNavClick("music")}
 				/> */}
-				<NavItem
-					to={"/assets/doc/Jason_Zubiate_UX_Resume.pdf"}
-					label={"Resume"}
-					current={currentPage === "resume"}
-					onClick={() => handleNavClick("resume")}
-				/>
-			</div>
-			<button
-				className={styles.btn}
-				onClick={() => {
-					setToggle(!toggle);
-				}}
-			>
-				{!toggle && (
-					<Image
-						className={styles.menu}
-						src={menu}
-						alt={"Menu"}
-						width={24}
-						height={24}
-						quality={100}
-					/>
-				)}
-				{toggle && (
-					<Image
-						className={styles.menu}
-						src={close}
-						alt={"Menu"}
-						width={20}
-						height={20}
-						quality={100}
-					/>
-				)}
-			</button>
-		</nav>
-	);
+        <NavItem
+          to={"/assets/doc/Jason_Zubiate_UX_Resume.pdf"}
+          label={"Resume"}
+          current={currentPage === "resume"}
+          onClick={() => handleNavClick("resume")}
+        />
+      </div>
+      <button
+        className={styles.btn}
+        onClick={() => {
+          setToggle(!toggle);
+        }}
+      >
+        {!toggle && (
+          <Image
+            className={styles.menu}
+            src={menu}
+            alt={"Menu"}
+            width={24}
+            height={24}
+            quality={100}
+          />
+        )}
+        {toggle && (
+          <Image
+            className={styles.menu}
+            src={close}
+            alt={"Menu"}
+            width={20}
+            height={20}
+            quality={100}
+          />
+        )}
+      </button>
+    </nav>
+  );
 }
